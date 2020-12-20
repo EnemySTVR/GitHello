b73b2f990816f03848b630142d2b252608c20cbf
EnemySTVR <shinkarev_dmitriy@mail.ru>
2020-12-20 23:37:51 +0300

    testetes

--- backback/Program.cs
+++ backback/Program.cs
@@ -6,6 +6,20 @@ namespace backback
     {
         static void Main(string[] args)
         {
+            int a = 5;
+
+            switch (a)
+            {
+                case 1:
+                case 2:
+                    break;
+                default: Console.WriteLine("hello!"); break;
+            }
+
+            for (int i = 0, a = 9; )
+            {
+
+            }
             Console.WriteLine("Hello World!");
         }
     }
